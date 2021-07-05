273d951ebdad1338d7b7f755428b1c9def87519d
AISWARYA B <74861291+AiswaryaB1@users.noreply.github.com>
2021-07-05 14:10:29 +0530

    Update app-routing.module.ts
    Routed components

--- MyGiftApp/src/app/app-routing.module.ts
+++ MyGiftApp/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './Register/guest.component';
+import { FilterscreenComponent } from './filterscreen/filterscreen.component';
 
 const routes: Routes = [
   {
@@ -14,8 +16,16 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
+    path: 'Register/guest-component', 
+    component: RegisterComponent
+  },
+  {
     path: 'login', 
     component: LoginComponent
+  },
+  {
+    path: 'filterscreen', 
+    component: FilterscreenComponent
   }
 ];
 
