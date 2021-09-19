db60a848f5ba7ca2527d5aa7daaa9eec115f68b2
samyu-ktha <samyukthak2000@gmail.com>
2021-09-19 12:58:48 +0530

    navbar and featured sections

--- MyGiftApp/src/app/app-routing.module.ts
+++ MyGiftApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { GuestComponent } from './guest/guest.component';
 import { FilterscreenComponent } from './filterscreen/filterscreen.component';
+import { MainscreenComponent } from './mainscreen/mainscreen.component';
 
 
 const routes: Routes = [
@@ -28,6 +29,10 @@ const routes: Routes = [
   {
     path: 'filterscreen',
     component: FilterscreenComponent
+  },
+  {
+    path: 'mainscreen',
+    component: MainscreenComponent
   }
 
 ];
