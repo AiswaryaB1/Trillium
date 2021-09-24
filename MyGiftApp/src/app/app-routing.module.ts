754127d56836f178ba835ea0ead2c97fbd54204c
AiswaryaB1 <aiswaryab.mec@gmail.com>
2021-09-24 21:09:59 +0530

    custom screen routing

--- MyGiftApp/src/app/app-routing.module.ts
+++ MyGiftApp/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { GuestComponent } from './guest/guest.component';
 import { FilterscreenComponent } from './filterscreen/filterscreen.component';
-
+import { MainscreenComponent } from './mainscreen/mainscreen.component';
+import { CustomComponent } from './custom/custom.component';
 
 const routes: Routes = [
   {
@@ -18,16 +19,24 @@ const routes: Routes = [
   },
 
   {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
     path: 'guest',
     component: GuestComponent
   },
   {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
     path: 'filterscreen',
     component: FilterscreenComponent
+  },
+  {
+    path: 'search',
+    component: MainscreenComponent
+  },
+  {
+    path: 'custom',
+    component: CustomComponent
   }
 
 ];
